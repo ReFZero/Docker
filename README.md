f4ef6e8db61e2e5f2ce036459fe09ef530f9174a
ReFZero <rafal.osypanka@gmail.com>
2024-11-09 12:05:14 +0100

    Inicjalizacja przykładowej bazy danych (PostgreSQL) wykorzystując obraz dockerowy

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Docker
-Konteneryzacja - Docker 
+Repozytorium zawiera przykłady konteneryzacji wykorzystując Docker 
+### Zmiany w ostatnim commicie:
+Inicjalizacja przykładowej bazy danych wykorzystując obraz dockerowy 
+#### Info:
+- Docker Compose ver. v2.29.2-desktop.2
+- Docker ver. 27.2.0, build 3ab4256
+#### Wykorzystane obrazy:
+- PostgreSQL ver. 17.0 
---
+++ db-init/init.sql
@@ -0,0 +1,56 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL
+    );
+
+INSERT INTO users (name, email) VALUES ('Dukey', 'dorange0@e-recht24.de');
+INSERT INTO users (name, email) VALUES ('Lari', 'lcud1@mac.com');
+INSERT INTO users (name, email) VALUES ('Alida', 'abollans2@ebay.com');
+INSERT INTO users (name, email) VALUES ('Janela', 'jreedy3@canalblog.com');
+INSERT INTO users (name, email) VALUES ('Sonnie', 'sandryszczak4@google.com.au');
+INSERT INTO users (name, email) VALUES ('Jewel', 'jzoren5@washington.edu');
+INSERT INTO users (name, email) VALUES ('Nessi', 'nsangwin6@geocities.jp');
+INSERT INTO users (name, email) VALUES ('Cristin', 'cmattioni7@reverbnation.com');
+INSERT INTO users (name, email) VALUES ('Sarene', 'sgarter8@japanpost.jp');
+INSERT INTO users (name, email) VALUES ('Idalia', 'ismaridge9@ed.gov');
+INSERT INTO users (name, email) VALUES ('Ferrell', 'flambrichta@smugmug.com');
+INSERT INTO users (name, email) VALUES ('Nicola', 'nbenzb@scribd.com');
+INSERT INTO users (name, email) VALUES ('Gardiner', 'groadc@blogspot.com');
+INSERT INTO users (name, email) VALUES ('Davide', 'dglowachad@e-recht24.de');
+INSERT INTO users (name, email) VALUES ('Aileen', 'anewbigginge@mlb.com');
+INSERT INTO users (name, email) VALUES ('Mathew', 'myulef@free.fr');
+INSERT INTO users (name, email) VALUES ('Larry', 'llibbeg@latimes.com');
+INSERT INTO users (name, email) VALUES ('Worth', 'wbiglinh@webmd.com');
+INSERT INTO users (name, email) VALUES ('Lani', 'lfearnsidei@bing.com');
+INSERT INTO users (name, email) VALUES ('Christoforo', 'cpauleitj@google.ru');
+INSERT INTO users (name, email) VALUES ('Bekki', 'bcoldmank@feedburner.com');
+INSERT INTO users (name, email) VALUES ('Rhea', 'rstonehaml@slideshare.net');
+INSERT INTO users (name, email) VALUES ('Olivero', 'ocarlessom@google.com.hk');
+INSERT INTO users (name, email) VALUES ('Bernardina', 'bbottomsn@bbc.co.uk');
+INSERT INTO users (name, email) VALUES ('Findlay', 'fheepso@discuz.net');
+INSERT INTO users (name, email) VALUES ('Sayres', 'sreihmp@scribd.com');
+INSERT INTO users (name, email) VALUES ('Gregorio', 'gvardyq@wp.com');
+INSERT INTO users (name, email) VALUES ('Saudra', 'saudrittr@oaic.gov.au');
+INSERT INTO users (name, email) VALUES ('Alexine', 'afadians@samsung.com');
+INSERT INTO users (name, email) VALUES ('Far', 'fgronewayt@slashdot.org');
+INSERT INTO users (name, email) VALUES ('Joann', 'jfillimoreu@netscape.com');
+INSERT INTO users (name, email) VALUES ('Christabel', 'cjeanneauv@economist.com');
+INSERT INTO users (name, email) VALUES ('Sanson', 'swallasw@dion.ne.jp');
+INSERT INTO users (name, email) VALUES ('Cthrine', 'cfawbertx@ask.com');
+INSERT INTO users (name, email) VALUES ('Berkley', 'browlstoney@hatena.ne.jp');
+INSERT INTO users (name, email) VALUES ('Cecilio', 'cfishez@bing.com');
+INSERT INTO users (name, email) VALUES ('Thomas', 'teveque10@rambler.ru');
+INSERT INTO users (name, email) VALUES ('Berti', 'bgosby11@live.com');
+INSERT INTO users (name, email) VALUES ('Chryste', 'ctembey12@youtube.com');
+INSERT INTO users (name, email) VALUES ('Chickie', 'cfoker13@twitter.com');
+INSERT INTO users (name, email) VALUES ('Baxter', 'bmendonca14@dailymotion.com');
+INSERT INTO users (name, email) VALUES ('Darren', 'doshirine15@woothemes.com');
+INSERT INTO users (name, email) VALUES ('Nevile', 'nvasyukhin16@mlb.com');
+INSERT INTO users (name, email) VALUES ('Waldo', 'wfeake17@shop-pro.jp');
+INSERT INTO users (name, email) VALUES ('Abdel', 'alefeuvre18@google.de');
+INSERT INTO users (name, email) VALUES ('Hobart', 'hsouthgate19@etsy.com');
+INSERT INTO users (name, email) VALUES ('Katalin', 'kobbard1a@whitehouse.gov');
+INSERT INTO users (name, email) VALUES ('Birgit', 'bgovenlock1b@deviantart.com');
+INSERT INTO users (name, email) VALUES ('Vincent', 'vpurveys1c@ox.ac.uk');
+INSERT INTO users (name, email) VALUES ('Joshua', 'jmccurry1d@nhs.uk');
